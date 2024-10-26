d01529ed01d8942d15b998c57dce83788e4bcec5
Hchoudry <102235101+HashimChoudry@users.noreply.github.com>
2024-10-27 00:23:22 +0100

    form completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/main/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center justify-center flex-col">
       <Header />
       <SignUpForm />
     </div>
--- src/components/form/signUpForm.tsx
+++ src/components/form/signUpForm.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import {  useToast } from "../hooks/use-toast";
+import { useToast } from "../hooks/use-toast";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import {
@@ -14,6 +14,7 @@ import {
   FormLabel,
   FormMessage,
 } from "../ui/form";
+import { Checkbox } from "../ui/checkbox";
 
 const SignUpForm = () => {
   const formSchema = z.object({
@@ -39,22 +40,19 @@ const SignUpForm = () => {
         ),
       country: z.string().default("United Kingdom").optional(),
     }),
-    // business_website: z.string().url("Invalid URL Format"),
+    business_website: z.string().url("Invalid URL Format"),
     // business_logo: z
-    //   .object({
-    //     file: z
-    //       .instanceof(File)
-    //       .refine((file) => file.size <= 5 * 1024 * 1024, {
-    //         message: "File Size Must be below 5mb",
-    //       })
-    //       .refine((file) => ["image/png", "image/jpeg"].includes(file.type), {
-    //         message: "File Must be a .png or a .jpeg",
-    //       }),
-    //   }).optional(),
+    //   .instanceof(File, { message: "Please Add a file" })
+    //   .refine((file) => file.size <= maxFileSize, {
+    //     message: "Ensure the file is less than 5MB",
+    //   })
+    //   .refine((file) => acceptedImageTypes.includes(file.type), {
+    //     message: "Please Use a .png or .jpeg file type",
+    //   }),
     // consent: z.boolean().refine((val) => val === true, {
     //   message: "You Must Consent To Your Logo Being Used",
     // }),
-    // email_consent: z.boolean(),
+    email_consent: z.boolean(),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -71,17 +69,17 @@ const SignUpForm = () => {
         postcode: "",
         country: "",
       },
-    //   business_website: "",
-    //   business_logo: undefined,
-    //   consent: false,
-    //   email_consent: false,
+      business_website: "",
+      //   business_logo: undefined,
+      //   consent:false,
+      email_consent: false,
     },
   });
 
-  const {toast} = useToast()
+  const { toast } = useToast();
 
   function onSubmit(data: z.infer<typeof formSchema>) {
-    console.log("toast made")
+    console.log("toast made");
     toast({
       title: "You Submitted The Following Values:",
       description: (
@@ -92,147 +90,244 @@ const SignUpForm = () => {
     });
   }
 
+  //   const fileRef = form.register("business_logo");
+
   return (
-    <section>
-      <div className="flex items-center justify-center">
+    <section className="w-4/5 flex items-center flex-col">
+        <div className="p-10">
+            <h2 className="font-semibold text-2xl">
+                Lets get Started
+            </h2>
+        </div>
+      <div className="flex items-center justify-center bg-neutral-200 text-black p-5 rounded-lg w-full">
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
-            className="w-2/3 space-y-5">
-            <FormField
-              control={form.control}
-              name="first_name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>First Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="text-black"
-                      placeholder="First Name"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="second_name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Second Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="text-black"
-                      placeholder="Second Name"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="contact_number"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Contact Number</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="text-black"
-                      placeholder="Contact Number"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField 
-            control={form.control}
-            name="business_name"
-            render={({field}) => (
-                <FormItem>
-                    <FormLabel>Business Name</FormLabel>
-                    <FormControl>
-                        <Input className="text-black" placeholder="Enter Business Name" {...field}/>
-                    </FormControl>
-                    <FormMessage />
-                </FormItem>
-            )}
-            />
-            {/* Business Adress fields */}
-            <FormField
-              control={form.control}
-              name="business_address.street"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>First Line Adress</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="text-black"
-                      placeholder="Endter The First Line of Address"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="business_address.city"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>City</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="text-black"
-                      placeholder="Endter The First Line of Address"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="business_address.postcode"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Postcode</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="text-black"
-                      placeholder="Endter The First Line of Address"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="business_address.country"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Country</FormLabel>
-                  <FormControl>
-                    <Input
-                      className="text-black"
-                      placeholder="Endter The First Line of Address"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button size={"lg"} type="submit">Submit</Button>
+            className="w-full space-y-10">
+            <div className="flex md:flex-row flex-col justify-between">
+              <div className="w-[48%]">
+                <div className="flex justify-between">
+                  <FormField
+                    control={form.control}
+                    name="first_name"
+                    render={({ field }) => (
+                      <FormItem className="w-[48%]">
+                        <FormLabel>First Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="text-black "
+                            placeholder="First Name"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="second_name"
+                    render={({ field }) => (
+                      <FormItem className="w-[48%]">
+                        <FormLabel>Second Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="text-black "
+                            placeholder="Second Name"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  control={form.control}
+                  name="contact_number"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Contact Number</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="text-black"
+                          placeholder="Contact Number"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="business_name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Business Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="text-black"
+                          placeholder="Enter Business Name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="business_email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Business Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="text-black"
+                          placeholder="Enter Your Business Email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex items-start flex-col">
+                {/* Business Address fields */}
+                <div className="flex flex-wrap justify-between">
+                  <FormField
+                    control={form.control}
+                    name="business_address.street"
+                    render={({ field }) => (
+                      <FormItem className="w-[48%]">
+                        <FormLabel>First Line Adress</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="text-black"
+                            placeholder="Enter The First Line of Address"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="business_address.city"
+                    render={({ field }) => (
+                      <FormItem className="w-[48%]">
+                        <FormLabel>City</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="text-black"
+                            placeholder="Enter The First Line of Address"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="business_address.postcode"
+                    render={({ field }) => (
+                      <FormItem className="w-[48%]">
+                        <FormLabel>Postcode</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="text-black"
+                            placeholder="Endter The First Line of Address"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="business_address.country"
+                    render={({ field }) => (
+                      <FormItem className="w-[48%]">
+                        <FormLabel>Country</FormLabel>
+                        <FormControl>
+                          <Input
+                            className="text-black"
+                            placeholder="Endter The First Line of Address"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  control={form.control}
+                  name="business_website"
+                  render={({ field }) => (
+                    <FormItem className="w-full mb-5">
+                      <FormLabel>Business Website</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="text-black"
+                          placeholder="Enter The Business Website"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {/* <FormField
+                      control={form.control}
+                      name="business_logo"
+                      render={({ field }) => {
+                        return (
+                          <FormItem>
+                            <FormLabel>File</FormLabel>
+                            <FormControl>
+                              <Input
+                                type="file"
+                                className="text-black"
+                                {...fileRef}
+                                onChange={(e) => {
+                                  field.onChange(e.target.files && e.target.files[0]);
+                                }}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        );
+                      }}
+                    /> */}
+                <FormField
+                  control={form.control}
+                  name="email_consent"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Checkbox
+                          checked={field.value}
+                          onCheckedChange={(checked) => field.onChange(checked)}
+                        />
+                      </FormControl>
+                      <FormLabel className="ml-2">
+                        I consent receiving emails from MBO
+                      </FormLabel>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button size={"lg"} type="submit" className="self-center">
+                  Submit
+                </Button>
+              </div>
+            </div>
           </form>
         </Form>
       </div>
